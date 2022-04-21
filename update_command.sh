f6186b02e00aaee070bbe27c0d999bc5fd319a36
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2022-04-21 23:18:03 +1000

    Updated dockerfile and update command

--- update_command.sh
+++ update_command.sh
@@ -1,13 +1,4 @@
+scp ros-supervisor.yml root@10.10.88.253:. && ssh root@10.10.88.253 "docker cp ros-supervisor.yml ros_supervisor:."
+scp docker-compose.yml root@10.10.88.253:. && ssh root@10.10.88.253 "docker cp docker-compose.yml ros_supervisor:."
 
-docker cp ros-supervisor.yml ros_supervisor:/supervisor/project/ros-supervisor.yml
-docker cp docker-compose.yml ros_supervisor:/supervisor/project/docker-compose.yml
-
-curl http://172.21.0.2:8080/cmd --include --header "Content-Type: application/json" --request "POST" --data '{"update_core": false, "update_services": true}'
-# curl http://192.168.0.101:8080/cmd --include --header "Content-Type: application/json" --request "POST" --data '{"update_core": false, "update_services": false}'
-
-# curl http://172.20.0.2:8080/health/liveness --silent --include --header "Content-Type: application/json" --request "GET"
-# res=$?
-# if test "$res" != "0"; then
-#    echo "the curl command failed with: $res"
-# fi
-# echo "the curl command failed with: $res"
+curl http://10.10.88.253:8080/cmd --include --header "Content-Type: application/json" --request "POST" --data '{"update_core": false, "update_services": true}'
