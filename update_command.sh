05fae9d16a6956fd36a34f183831e2fea12d40ce
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2022-04-22 15:44:54 +1000

    Updated update command

--- update_command.sh
+++ update_command.sh
@@ -1,4 +1,14 @@
-scp ros-supervisor.yml root@10.10.88.253:. && ssh root@10.10.88.253 "docker cp ros-supervisor.yml ros_supervisor:."
-scp docker-compose.yml root@10.10.88.253:. && ssh root@10.10.88.253 "docker cp docker-compose.yml ros_supervisor:."
+#!/bin/bash
 
-curl http://10.10.88.253:8080/cmd --include --header "Content-Type: application/json" --request "POST" --data '{"update_core": false, "update_services": true}'
+IP_ADDRESS=$1
+
+if [ "$IP_ADDRESS" = "localhost" ]; then
+    echo "Development"
+    docker cp ros-supervisor.yml ros_supervisor:/supervisor/project/
+    docker cp docker-compose.yml ros_supervisor:/supervisor/project/
+else
+    scp ros-supervisor.yml root@$IP_ADDRESS:. && ssh root@$IP_ADDRESS "docker cp ros-supervisor.yml ros_supervisor:/supervisor/project/"
+    scp docker-compose.yml root@$IP_ADDRESS:. && ssh root@$IP_ADDRESS "docker cp docker-compose.yml ros_supervisor:/supervisor/project/"
+fi
+
+curl http://$IP_ADDRESS:8080/cmd --include --header "Content-Type: application/json" --request "POST" --data '{"update_core": false, "update_services": true}'
